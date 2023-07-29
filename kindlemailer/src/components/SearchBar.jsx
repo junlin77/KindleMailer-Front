07053757bc9116afcb1dc1f8afba7a5a1d775f4d
junlin77 <junlin.liow7@gmail.com>
2023-07-29 21:22:49 +0800

    Add input icons

--- kindlemailer/src/components/SearchBar.jsx
+++ kindlemailer/src/components/SearchBar.jsx
@@ -1,16 +1,41 @@
 import {
     Input,
+    InputGroup,
+    InputLeftElement,
     Stack,
   } from '@chakra-ui/react'
+
+import { BiBook } from 'react-icons/bi';
+import { AiOutlineUser, AiOutlineFile, AiOutlineMail } from 'react-icons/ai';
 
 function SearchBar() {
     return (
         <form>
         <Stack spacing={3} direction="row" align="center">
-            <Input variant="outline" type="text" placeholder="Title" />
-            <Input variant="outline" type="text" placeholder="Author"/>
-            <Input variant="outline" type="text" placeholder="Extension"/>
-            <Input variant="outline" type="text" placeholder="Email"/>
+            <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                    <BiBook />
+                </InputLeftElement>
+                <Input variant="outline" type="text" placeholder="Title" />
+            </InputGroup>
+            <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                    <AiOutlineUser />
+                </InputLeftElement>
+                <Input variant="outline" type="text" placeholder="Author"/>
+            </InputGroup>
+            <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                    <AiOutlineFile />
+                </InputLeftElement>
+                <Input variant="outline" type="text" placeholder="Extension"/>
+            </InputGroup>
+            <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                    <AiOutlineMail />
+                </InputLeftElement>
+                <Input variant="outline" type="text" placeholder="Email"/>
+            </InputGroup>
         </Stack>
         </form>
     );
