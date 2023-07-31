05fd389ba2114c8f4d991e2772b3fd6b985909a8
junlin77 <junlin.liow7@gmail.com>
2023-07-31 16:59:32 +0800

    Resolve table overflow

--- kindlemailer/src/components/BookRow.jsx
+++ kindlemailer/src/components/BookRow.jsx
@@ -6,26 +6,22 @@ import {
 import { BsSend } from 'react-icons/bs';
 
 function BookRow({ Book }) {
-    <span style={{ color: 'red' }}>
-      {Book.name}
-    </span>;
-
   return (
-    <Tr>
-      <Td>{Book.Author}</Td>
-      <Td>{Book.Title}</Td>
-      <Td>{Book.Publisher}</Td>
-      <Td>{Book.Year}</Td>
-      <Td>{Book.Pages}</Td>
-      <Td>{Book.Language}</Td>
-      <Td>{Book.Size}</Td>
-      <Td>{Book.Extension}</Td>
-      <Td>
-        <button onClick={() => handleSend(Book.id)}>
-          <BsSend />
-        </button>
-      </Td>
-    </Tr>
+      <Tr>
+        <Td>{Book.Author}</Td>
+        <Td>{Book.Title}</Td>
+        <Td>{Book.Publisher}</Td>
+        <Td>{Book.Year}</Td>
+        <Td>{Book.Pages}</Td>
+        <Td>{Book.Language}</Td>
+        <Td>{Book.Size}</Td>
+        <Td>{Book.Extension}</Td>
+        <Td>
+          <button onClick={() => handleSend(Book.id)}>
+            <BsSend />
+          </button>
+        </Td>
+      </Tr>
   );
 }
 
--- kindlemailer/src/components/BookTable.jsx
+++ kindlemailer/src/components/BookTable.jsx
@@ -13,31 +13,27 @@ function BookTable({ Books }) {
   
     Books.forEach((Book) => {
       rows.push(
-        <BookRow
-          Book={Book}
-          key={Book.name} />
+        <BookRow Book={Book} />
       );
     });
   
     return (
-      <TableContainer>
-        <Table variant="simple" colorScheme="light-grey">
-          <Thead>
-            <Tr>
-                <Th>Author</Th>
-                <Th>Title</Th>
-                <Th>Publisher</Th>
-                <Th>Year</Th>
-                <Th>Pages</Th>
-                <Th>Language</Th>
-                <Th>Size</Th>
-                <Th>Extension</Th>
-                <Th></Th>
-            </Tr>
-          </Thead>
-          <Tbody>{rows}</Tbody>
-        </Table>
-      </TableContainer>
+      <Table variant="simple" colorScheme="light-grey">
+        <Thead>
+          <Tr>
+              <Th>Author</Th>
+              <Th>Title</Th>
+              <Th>Publisher</Th>
+              <Th>Year</Th>
+              <Th>Pages</Th>
+              <Th>Language</Th>
+              <Th>Size</Th>
+              <Th>Extension</Th>
+              <Th></Th>
+          </Tr>
+        </Thead>
+        <Tbody>{rows}</Tbody>
+      </Table>
     );
   }
 
