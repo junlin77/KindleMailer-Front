ac510fc9628b3cb583ac38c481a449cf3e73cdf0
junlin77 <junlin.liow7@gmail.com>
2023-08-04 22:17:51 +0800

    Add sign in with google button

--- kindlemailer/src/components/Header.jsx
+++ kindlemailer/src/components/Header.jsx
@@ -1,14 +1,26 @@
+import { useGoogleLogin } from '@react-oauth/google';
+import { Button } from '@chakra-ui/react';
 import '../styles/Header.css';
 import DarkModeToggle from './DarkModeToggle'; 
 
 const Header = () => {
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log('Login Failed:', error)
+  });
+  
   return (
     <div className="header">
       <div className="header-text">
         <h5>KindleMailer</h5>
-        <p>Email searched books to your Kindle!</p>
+        <p>Send books to your Kindle with ease.</p>
       </div>
-      <DarkModeToggle />
+      <div className="header-controls">
+        <Button variant='ghost' onClick={() => login()} className="header-button">
+          Sign in with Google 🚀
+        </Button>
+        <DarkModeToggle />
+      </div>
     </div>
   );
 };
--- kindlemailer/src/main.jsx
+++ kindlemailer/src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <ColorModeScript initialColorMode="light" />
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+  <GoogleOAuthProvider clientId="1092112106425-fk1srkper0b3cqsthppbb8e21d2ntol7.apps.googleusercontent.com">
+    <React.StrictMode>
+      <ChakraProvider>
+        <ColorModeScript initialColorMode="light" />
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 )
