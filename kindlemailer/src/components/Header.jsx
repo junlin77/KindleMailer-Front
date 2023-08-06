01ddf9d0e90c17a9ac29885a7da32bc35d67ad25
junlin77 <junlin.liow7@gmail.com>
2023-08-06 15:36:03 +0800

    Add email settings modal

--- kindlemailer/src/components/Header.jsx
+++ kindlemailer/src/components/Header.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useGoogleLogin, googleLogout } from '@react-oauth/google';
-import { Button } from '@chakra-ui/react';
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter, ModalCloseButton } from '@chakra-ui/react'; 
 import '../styles/Header.css';
 import DarkModeToggle from './DarkModeToggle';
 import axios from 'axios';
-import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
+import { Menu, MenuButton, MenuList, MenuItem, Input, FormControl, FormLabel } from '@chakra-ui/react';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import Swal from 'sweetalert2';
 
 const Header = () => {
   const [userProfile, setUserProfile] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false); 
+  const [kindleEmail, setKindleEmail] = useState('');
+  const [currentKindleEmail, setCurrentKindleEmail] = useState('');
 
   const login = useGoogleLogin({
     onSuccess: tokenResponse => {
@@ -21,16 +26,70 @@ const Header = () => {
     try {
       const response = await axios.post('http://127.0.0.1:8000/api/login/', { access_token });
       console.log('Backend response:', response.data);
-      setUserProfile(response.data); // Set the user profile from the backend response
+      setUserProfile(response.data);
+      setKindleEmail(response.data.kindle_email);
     } catch (error) {
       console.error('Error sending token:', error);
     }
   };
 
+  useEffect(() => {
+    // Fetch the current Kindle email and update the state
+    const fetchCurrentKindleEmail = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/get_kindle_email/', {
+          params: { user_id: userProfile.user_id },
+        });
+        console.log('Current Kindle email:', response.data.kindle_email);
+        setCurrentKindleEmail(response.data.kindle_email);
+      } catch (error) {
+        console.error('Error fetching Kindle email:', error);
+      }
+    };
+    // Check if userProfile is not null before fetching
+    if (userProfile) {
+      fetchCurrentKindleEmail();
+    }
+  }, [isModalOpen, userProfile]); 
+  
   const logOut = () => {
     googleLogout();
-    setUserProfile(null); 
+    setUserProfile(null);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
     setIsModalOpen(false);
+  };
+
+  const setKindleEmailBackend = async () => {
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/set_kindle_email/', {
+        user_id: userProfile.user_id, 
+        kindle_email: kindleEmail,
+      });
+      console.log('Kindle email set:', response.data);
+      closeModal();
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Success',
+        text: 'Kindle email updated successfully.',
+      });
+  
+    } catch (error) {
+      console.error('Error setting Kindle email:', error);
+      closeModal();
+
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'An error occurred while updating Kindle email.',
+      });
+    }
   };
 
   return (
@@ -44,18 +103,60 @@ const Header = () => {
         {userProfile ? (
           <div className="user-profile">
             <Menu>
-              <MenuButton as={Button} variant={'link'}>
-                <img
-                  src={userProfile.profile_picture}
-                  alt="Profile"
-                  className="profile-picture"
-                />
+              <MenuButton as={Button} variant={'link'} className='menu-button'>
+                <div className="profile-icon-container">
+                  <img
+                    src={userProfile.profile_picture}
+                    alt="Profile"
+                    className="profile-picture"
+                  />
+                  <ChevronDownIcon className="chevron-icon" />
+                </div>
               </MenuButton>
               <MenuList>
-                <MenuItem>📩 Set Kindle Email</MenuItem>
+                <MenuItem onClick={openModal}>📩 Set Kindle Email</MenuItem>
                 <MenuItem onClick={logOut}>👋 Log out</MenuItem>
               </MenuList>
             </Menu>
+            {/* Modal to set Kindle email */}
+            <Modal isOpen={isModalOpen} onClose={closeModal}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Kindle Email Settings</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+              {currentKindleEmail !== '' && (
+              <FormControl>
+                <FormLabel>Current address</FormLabel>
+                <Input
+                  value={currentKindleEmail}
+                  isReadOnly
+                />
+              </FormControl>
+            )}
+
+              <FormControl mt={4}>
+              <FormLabel>
+                {currentKindleEmail === ''
+                  ? "Configure your Kindle Email address"
+                  : "New address"}
+              </FormLabel>
+                <Input
+                  placeholder="Enter your new Kindle email"
+                  onChange={(e) => {setKindleEmail(e.target.value)}}
+                />
+              </FormControl>
+              </ModalBody>
+              <ModalFooter>
+                <Button variant="ghost" onClick={setKindleEmailBackend}>
+                  Save
+                </Button>
+                <Button colorScheme="blue" mr={3} onClick={closeModal}>
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
           </div>
         ) : (
           <Button variant='ghost' onClick={login} className="header-button">
