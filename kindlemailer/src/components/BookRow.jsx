cb6e54b791f0998818a8ef6e198948f2af42fec8
junlin77 <junlin.liow7@gmail.com>
2023-11-03 18:05:05 +0800

    Responsive layout

--- kindlemailer/src/components/BookRow.jsx
+++ kindlemailer/src/components/BookRow.jsx
@@ -6,7 +6,6 @@ import {
   Button,
   Spinner,
   Flex,
-  Image,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -15,8 +14,18 @@ import {
   ModalBody,
   ModalCloseButton,
   useDisclosure,
+  Stack,
+  Heading,
+  Text,
+  Card, 
+  CardHeader, 
+  CardBody, 
+  CardFooter,
+  useBreakpointValue,
+  Box,
 } from '@chakra-ui/react';
 import { BsSend } from 'react-icons/bs';
+import { MdOutlineImageNotSupported } from 'react-icons/md';
 import Swal from 'sweetalert2';
 import '../styles/BookRow.css';
 
@@ -24,6 +33,10 @@ function BookRow({ Book, email }) {
   const [isLoading, setIsLoading] = useState(false);
   const [coverImage, setCoverImage] = useState('');
   const [details, setDetails] = useState(null);
+
+  const isMobile = useBreakpointValue({ base: true, sm: false });
+  const [imageWidth, setImageWidth] = useState(0);
+  const [imageHeight, setImageHeight] = useState(0);
 
   useEffect(() => {
     console.log('Book prop changed:', Book);
@@ -40,7 +53,12 @@ function BookRow({ Book, email }) {
   
     // Construct the API request URL for Open Library Covers API
     const apiUrl = `https://covers.openlibrary.org/b/${coverId}/${coverValue}-M.jpg`;
-  
+    const img = new Image();
+    img.src = apiUrl;
+    img.onload = function () {
+      setImageWidth(this.width);
+      setImageHeight(this.height); 
+    };
     setCoverImage(apiUrl);
   };  
 
@@ -77,7 +95,6 @@ function BookRow({ Book, email }) {
     axios
       .post(apiUrl, data)
       .then((response) => {
-        // The POST request is successful, show the success modal
         console.log('POST request successful:', response);
         setIsLoading(false);
         onClose();
@@ -113,60 +130,113 @@ function BookRow({ Book, email }) {
     }
   }, [isOpen]);
 
+  const isIsbn = Book.isbn !== null;
+
   return (
-    <Tr onClick={onOpen}>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Book Details</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            {details ? (
-              <div className='details-container'>
-                {coverImage && <Image src={coverImage} alt="Cover" />}
-                <div className='details-text'>
-                  <p>Author: {details.author}</p>
-                  <p>Title: {details.title}</p>
-                  <p>Filesize: {details.filesize}</p>
-                  <p>ISBN: {details.isbn}</p>
-                  <p>Extension: {details.extension}</p>
-                  <p>Source: {details.source}</p>
-                </div>
-              </div>
-            ) : (
-              <p>Currently unavailable</p>
-            )}
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
+    isMobile ? (
+      <Card variant='outline' overflow='hidden' gap='8' direction={{ base: 'row' }} p={2} align='center'>
+        {isIsbn && imageHeight > 1 && imageWidth > 1 ? (
+          <img
+            id='imageElement'
+            style={{
+              objectFit: 'cover',
+              maxWidth: { base: '30%', sm: '100px' },
+              marginLeft: '2rem'
+            }}
+            src={coverImage}
+            alt={Book.title}
+            onError={(e) => {
+              e.target.style.display = 'none';
+            }}
+          />
+        ) : (
+          <div style={{ marginLeft: '4rem', marginRight: '2rem' }}> {/* Add left margin to the icon */}
+              <MdOutlineImageNotSupported size={100} color="gray" />
+            </div>
+        )}
+        <Stack mt>
+          <CardBody align='left'>
+            <Text fontSize='xl' fontWeight='semibold'>{Book.title}</Text>
+            <Text fontSize='m' color='gray.500'>Author: {Book.author}</Text>
+            <Text fontSize='m' color='gray.500'>Year: {Book.year}</Text>
+            <Text fontSize='m' color='gray.500'>Language: {Book.language}</Text>
+            <Text fontSize='m' color='gray.500'>ISBN: {Book.isbn}</Text>
+            <Text fontSize='m' color='gray.500'>Extension: {Book.extension}</Text>
+          </CardBody>
+          <CardFooter>
             {isLoading ? (
-              <Flex justify="center" align="center">
-                <Spinner size="md" color="blue.500" />
+              <Flex justify='center' align='center'>
+                <Spinner size='md' color='blue.500' />
               </Flex>
             ) : (
-              <Button onClick={() => handleSend(Book)}>
-                <BsSend />  Send to Kindle
-              </Button>
+              <Flex justify='right' align='right'>
+                <Button onClick={async () => {
+                  await fetchDetails();
+                  if (details) {
+                    handleSend(Book);
+                  }
+                }}>
+                  <BsSend /> Send to Kindle
+                </Button>
+              </Flex>
             )}
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+          </CardFooter>
+        </Stack>
+      </Card>
+    ) : (
+      <Tr onClick={onOpen}>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Book Details</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              {details ? (
+                <div className='details-container'>
+                  {coverImage && <img src={coverImage} alt="Cover" />}
+                  <div className='details-text'>
+                    <p>Author: {details.author}</p>
+                    <p>Title: {details.title}</p>
+                    <p>Filesize: {details.filesize}</p>
+                    <p>ISBN: {details.isbn}</p>
+                    <p>Extension: {details.extension}</p>
+                    <p>Source: {details.source}</p>
+                  </div>
+                </div>
+              ) : (
+                <p>Currently unavailable</p>
+              )}
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+              {isLoading ? (
+                <Flex justify="center" align="center">
+                  <Spinner size="md" color="blue.500" />
+                </Flex>
+              ) : (
+                <Button onClick={() => handleSend(Book)}>
+                  <BsSend />  Send to Kindle
+                </Button>
+              )}
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
 
-      <Td>
-        {/* Render the cover image */}
-        {coverImage && <Image src={coverImage} alt="Cover"/>}
-      </Td>
-      <Td>{Book.author}</Td>
-      <Td>{Book.title}</Td>
-      <Td>{Book.publisher}</Td>
-      <Td>{Book.year}</Td>
-      <Td>{Book.language}</Td>
-      <Td>{Book.filesize}</Td>
-      <Td>{Book.isbn}</Td>
-      <Td>{Book.extension}</Td>
-    </Tr>
+        <Td>
+          {coverImage && <img src={coverImage} alt="Cover"/>}
+        </Td>
+        <Td>{Book.author}</Td>
+        <Td>{Book.title}</Td>
+        <Td>{Book.publisher}</Td>
+        <Td>{Book.year}</Td>
+        <Td>{Book.language}</Td>
+        <Td>{Book.filesize}</Td>
+        <Td>{Book.isbn}</Td>
+        <Td>{Book.extension}</Td>
+      </Tr>
+    )
   );
 }
 
--- kindlemailer/src/components/BookTable.jsx
+++ kindlemailer/src/components/BookTable.jsx
@@ -6,10 +6,13 @@ import {
     Tr,
     Th,
     Td, 
+    Box,
+    useBreakpointValue,
   } from '@chakra-ui/react'
 import '../styles/BookTable.css';
 
 function BookTable({ Books, email }) {
+    const isMobile = useBreakpointValue({ base: true, sm: false });
     const rows = [];
   
     Books.forEach((Book) => {
@@ -19,32 +22,44 @@ function BookTable({ Books, email }) {
     });
   
     return (
-      <Table variant="simple" colorScheme="light-grey">
-        <Thead>
-          <Tr>
-            <Th className="empty-cell"></Th>
-            <Th>Author</Th>
-            <Th>Title</Th>
-            <Th>Publisher</Th>
-            <Th>Year</Th>
-            <Th>Language</Th>
-            <Th>Size</Th>
-            <Th>ISBN</Th>
-            <Th>Extension</Th>
-          </Tr>
-        </Thead>
-        <Tbody className='table-body'>
+      isMobile ? (
+        <Box className='mobile-content'>
           {Books.length === 0 ? (
-            <Tr className='no-data-row'>
-              <Td colSpan='10' textAlign='center'>No data</Td>
-            </Tr>
+            <p>No data</p>
           ) : (
             Books.map((Book) => (
               <BookRow Book={Book} email={email} />
             ))
           )}
-        </Tbody>
-      </Table>
+        </Box>
+      ) : (
+        <Table variant="simple" colorScheme="light-grey">
+          <Thead>
+            <Tr>
+              <Th className="empty-cell"></Th>
+              <Th>Author</Th>
+              <Th>Title</Th>
+              <Th>Publisher</Th>
+              <Th>Year</Th>
+              <Th>Language</Th>
+              <Th>Size</Th>
+              <Th>ISBN</Th>
+              <Th>Extension</Th>
+            </Tr>
+          </Thead>
+          <Tbody className='table-body'>
+            {Books.length === 0 ? (
+              <Tr className='no-data-row'>
+                <Td colSpan='10' textAlign='center'>No data</Td>
+              </Tr>
+            ) : (
+              Books.map((Book) => (
+                <BookRow Book={Book} email={email} />
+              ))
+            )}
+          </Tbody>
+        </Table>
+      )
     );
   }
 
